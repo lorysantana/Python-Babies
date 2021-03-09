1595a72f64002fc03af5c63ce87f0402729f1808
Lory <lorysantana2106@gmail.com>
2021-03-09 15:45:03 -0300

    Create medias.py

---
+++ medias.py
@@ -0,0 +1,11 @@
+print("Olá! Para calcular a média, digite os dados abaixo: ")
+
+n1 = input("Digite a primeira nota: ")
+n2 = input("Digite a segunda nota: ")
+n3 = input("Digite a terceira nota: ")
+n4 = input("Digite a quarta nota: ")
+
+M = (float(n1)+float(n2)+float(n3)+float(n4)) / 4 
+
+print("A média aritmética é", M)
+
